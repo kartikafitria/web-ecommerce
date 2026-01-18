e906b9cdd58fbc714f813a7d0f019c397f72eaf3
Kartika Nur Fitria <kartikaftriaa@gmail.com>
2026-01-19 02:00:47 +0700

    fix: handle null API response

--- app/services/bank.service.ts
+++ app/services/bank.service.ts
@@ -2,5 +2,12 @@ import { fetchAPI } from "../lib/api";
 import { Bank } from "../types";
 
 export const getAllBanks = async (): Promise<Bank[]> => {
-  return await fetchAPI<Bank[]>("/banks");
+  const res = await fetchAPI<Bank[]>("/banks");
+
+  if (!res) {
+    console.error("Failed to fetch banks");
+    return [];
+  }
+
+  return res;
 };
